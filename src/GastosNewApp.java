e5742cfc013153e2326e062f2e690f196922d500
andreluizsk <Andreluizspfc@hotmail.com>
2022-07-04 16:41:59 -0300

    Incluindo a branchc custosNewApp

---
+++ src/GastosNewApp.java
@@ -0,0 +1,14 @@
+public class GastosNewApp {
+    public static void main(String[] args) {
+        int forro = 650;
+        System.out.println("valor do forro " + forro);
+        int bancada = 600;
+        System.out.println("valor do bancada " + bancada );
+        int pintura = 400;
+        System.out.println("valor da pintura " + pintura );
+        int lampadas = 75;
+        System.out.println("valor das lampadas " + lampadas);
+        int total = forro + bancada + pintura + lampadas;
+        System.out.println("valor do total " + total);
+    }
+}
