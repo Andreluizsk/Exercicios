d3a0cc6d32860aca1b125e51b8628cfd943bc54e
Andre <Andreluizspfc@hotmail.com>
2022-07-04 12:10:02 -0300

    Primeiro Commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+public class Main {
+    public static void main(String[] args) {
+
+        String valor = JOptionPane.showInputDialog("Informe o Valor desejado");
+        int valorInteiro = Integer.parseInt(valor);
+        System.out.println(valorInteiro);
+
+
+        }
+}
