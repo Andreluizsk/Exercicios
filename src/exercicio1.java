45b745356de68cb5ff8863fbd69b8ffebf9d6c78
andreluizsk <Andreluizspfc@hotmail.com>
2022-07-06 15:57:41 -0300

    Commit do primeiro exercicio

---
+++ src/exercicio1.java
@@ -0,0 +1,9 @@
+public class exercicio1 {
+    public static void main(String[] args) {
+        int a = 150;
+        while (a <= 300) {
+            System.out.println(a);
+            a = a + 1;
+        }
+    }
+}
