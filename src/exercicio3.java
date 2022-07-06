e9ba80f46abdce3122e8c7dd455043fcc7256230
andreluizsk <Andreluizspfc@hotmail.com>
2022-07-06 16:21:38 -0300

    Commit do terceiro exercicio

---
+++ src/exercicio3.java
@@ -0,0 +1,9 @@
+public class exercicio3 {
+    public static void main(String[] args) {
+    int i = 0;
+    while (i < 1000) {
+        System.out.println(i);
+        i = i + 3;
+        }
+    }
+}
