6e171a076025bf014595145d52fe1ee5adfcb68c
andreluizsk <Andreluizspfc@hotmail.com>
2022-07-06 16:13:43 -0300

    Commit segundo exercicio

---
+++ src/exercicio2.java
@@ -0,0 +1,8 @@
+public class exercicio2 {
+    public static void main(String[] args) {
+        for (int x = 0, y = 0; x <= 1000; y++) {
+            x = x + y;
+            System.out.println(x);
+        }
+    }
+}
