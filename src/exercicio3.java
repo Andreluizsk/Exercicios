7be982ba077dfc6ec09f9af784219493162c21b8
andreluizsk <Andreluizspfc@hotmail.com>
2022-07-07 10:00:40 -0300

    Resolvendo o exercicio com o For

--- src/exercicio3.java
+++ src/exercicio3.java
@@ -1,9 +1,19 @@
 public class exercicio3 {
+    /*  public static void main(String[] args) {
+      int i = 0;
+      while (i < 100) {
+          System.out.println(i);
+          i = i + 3;
+          }
+      }*/
     public static void main(String[] args) {
-    int i = 0;
-    while (i < 1000) {
-        System.out.println(i);
-        i = i + 3;
+        int x = 1;
+
+        for (int i = x; i <= 100; i++) {
+            if (i %3 == 0 ) {
+                System.out.println(i);
+
+            }
         }
     }
 }
