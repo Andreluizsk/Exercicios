dd5f08374104aee23922eeac0f0c0b7a82546305
andreluizsk <Andreluizspfc@hotmail.com>
2022-07-11 17:47:31 -0300

    Commit do somaIdades

--- src/somaIdades.java
+++ src/somaIdades.java
@@ -1,2 +1,18 @@
+import javax.swing.*;
+
 public class somaIdades {
+    public static void main(String[] args) {
+        String quantos = JOptionPane.showInputDialog(null, "Informe quantas idade quer saber?" + "\n");
+        int numeroDePessoas = Integer.parseInt(quantos);
+        int quantidade = numeroDePessoas;
+        int soma = 0;
+        for (int i = 0; i < quantidade; i++) {
+
+            String idade1 = JOptionPane.showInputDialog(null, "Informe a Idade " + "\n");
+            int idadeInteiro1 = Integer.parseInt(idade1);
+            System.out.println(idadeInteiro1);
+            soma = soma + idadeInteiro1;
+        }
+        System.out.println("O valor total de todas as idades é: " + soma);
+    }
 }
