87734415a66c75d3e25d1890870e8b105e28d40e
andreluizsk <Andreluizspfc@hotmail.com>
2022-07-12 15:34:09 -0300

    Commit do codigo com todas funcionalidades

--- src/somaIdades.java
+++ src/somaIdades.java
@@ -1,18 +1,29 @@
 import javax.swing.*;
+import java.lang.*;
 
 public class somaIdades {
     public static void main(String[] args) {
-        String quantos = JOptionPane.showInputDialog(null, "Informe quantas idade quer saber?" + "\n");
-        int numeroDePessoas = Integer.parseInt(quantos);
-        int quantidade = numeroDePessoas;
         int soma = 0;
-        for (int i = 0; i < quantidade; i++) {
-
-            String idade1 = JOptionPane.showInputDialog(null, "Informe a Idade " + "\n");
-            int idadeInteiro1 = Integer.parseInt(idade1);
-            System.out.println(idadeInteiro1);
-            soma = soma + idadeInteiro1;
+        String minhaString = JOptionPane.showInputDialog(null, "Informe quantas idade quer saber?" + "\n");
+        boolean eUmInteiro = minhaString.matches("-?\\d+");
+        int quantidade = 0;
+        if (eUmInteiro == true) {
+            quantidade = Integer.parseInt(minhaString);
+        } else {
+            System.out.println("Dado Invalido");
         }
-        System.out.println("O valor total de todas as idades é: " + soma);
+        for (int i = 0; i < quantidade; i++) {
+            String idade1 = JOptionPane.showInputDialog(null, "Informe a Idade " + "\n");
+            boolean idade = idade1.matches("-?\\d+");
+            if (idade == true) {
+                int idadeInteiro1 = Integer.parseInt(idade1);
+                System.out.println("A idade digitada é: " + idadeInteiro1);
+                soma = soma + idadeInteiro1;
+            } else {
+                System.out.println("Dado Invalido");
+            }
+
+            System.out.println("O valor total de todas as idades é: " + soma);
+        }
     }
 }
